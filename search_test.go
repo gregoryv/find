e1a324df182f41221750722b2f85a16fd035b4e5
Gregory Vincic <g@7de.se>
2017-11-11 07:55:10 +0100

    Separate test directory

--- search_test.go
+++ search_test.go
@@ -1,41 +1,84 @@
-package find
+package find_test
 
 import (
 	"testing"
 	"regexp"
+	"os"
+	"io/ioutil"
+	"path"
+		"fmt"
+	"github.com/gregoryv/find"
 )
 
-func TestBy(t *testing.T) {
+var testRoot string // set by TestSuite
+
+func TestSuite(t *testing.T) {
+	var err error
+	// Setup directory structure for tests
+	testRoot, err = ioutil.TempDir("", "search_test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	content := []struct{
+		path string
+		isDir bool
+	}{
+		{"cars/", true},
+		{"a.txt", false},
+		{"b.txt", false},
+	}
+	for _, c := range content {
+		full := path.Join(testRoot, c.path)
+		if c.isDir {
+			os.MkdirAll(full, 0755)
+		} else {
+			ioutil.WriteFile(full, []byte{}, 0755)
+		}
+	}
+	// Run tests
+	os.Chdir(testRoot)
+	t.Run("By", testBy)
+	t.Run("ByName", testByName)
+}
+
+
+func exampleByName() {
+	result, _ := find.ByName("*.txt", ".")
+	fmt.Printf("%v", result)
+	//output:[a.txt b.txt]
+}
+
+func testBy(t *testing.T) {
 	data := []struct {
-		m Matcher
+		m find.Matcher
 		root string
 		count int
 	}{
-		{NewRegexp(regexp.MustCompile(`.*\.md`)), ".", 1},
+		{find.NewRegexp(regexp.MustCompile(`.*\.txt`)), ".", 2},
 	}
 	for _, d := range data {
-		result, _ := By(d.m, d.root)
+		result, _ := find.By(d.m, d.root)
 		if len(result) != d.count {
 			t.Errorf("By(%q, %q) expected to find %v files, found %v", d.m, d.root, d.count, len(result))
 		}
 	}
 }
 
-func TestByName(t *testing.T) {
+func testByName(t *testing.T) {
 	data := []struct {
 		pattern string
 		root    string
 		count   int
 		ok      bool
 	}{
-		{"search.go", ".", 1, true},
-		{"search*", "", 2, true}, // no directory means "."
+		{"a.txt", ".", 1, true},
+		{"*.txt", "", 2, true}, // no directory means "."
 		{"x", ".", 0, false},
 		{"whatever", "nosuchdir", 0, false},
 		{"", ".", 0, false},
 	}
 	for _, d := range data {
-		result, err := ByName(d.pattern, d.root)
+		result, err := find.ByName(d.pattern, d.root)
 		if d.ok && err != nil {
 			t.Errorf("ByName(%q, %q): %s", d.pattern, d.root, err)
 		}
